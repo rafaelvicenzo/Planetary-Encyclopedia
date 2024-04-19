eb244cc060592848edef23bed596ef70c2469502
rafaelvicenzo <rafaellordes.lordes@gmail.com>
2024-04-18 21:54:01 -0300

    APOD Implementation

--- apod.js
+++ apod.js
@@ -1,11 +1,36 @@
 'use strict'
 
-fetch('https://api.nasa.gov/planetary/apod?api_key=eDcGmc68SHznNH1e1w9QDxnEbcnu3x2JOJnINsa2')
+const apiKEY = 'eDcGmc68SHznNH1e1w9QDxnEbcnu3x2JOJnINsa2';
+const dataSelect = document.getElementById('start');
+dataSelect.addEventListener('change', function () {
+    const selectDate = this.value;
+
+
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKEY}&date=${selectDate}`)
 .then(response => { 
     return response.json()
 })
 .then(data => { 
+    const apodIMG = document.querySelector('#apod-img');
+    const apodText = document.querySelector('#apod-text');
+
+    apodIMG.src = data.url;
+    apodText.textContent = data.explanation;
+
     console.log(data);
 })
 .catch(error => { console.log(error)
+})
 });
+
+const today = new Date();
+    const todayISO = today.toISOString().split('T')[0];
+    loadApodImage(todayISO);
+
+    dataSelect.addEventListener('change', function () {
+        const selectedDate = this.value;
+        loadApodImage(selectedDate);
+    });
+
+
+
